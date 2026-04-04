a054d9fbd007b188d3f657bc4dbbe5c313fd1b0c
Jianjun Chen <sychenjianjun@gmail.com>
2026-04-04 07:10:13 +0000

    fix: create cmd/haystack/main.go and fix vscode launch config

---
+++ cmd/haystack/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	_ "net/http/pprof"
+	"path/filepath"
+
+	"github.com/codetrek/haystack/client"
+	"github.com/codetrek/haystack/conf"
+	"github.com/codetrek/haystack/server"
+	"github.com/codetrek/haystack/shared/running"
+)
+
+var version = "dev"
+
+// Global help flags
+var (
+	helpShort = flag.Bool("h", false, "Show help")
+	helpLong  = flag.Bool("help", false, "Show help")
+)
+
+func init() {
+	flag.Usage = func() { client.PrintUsage() }
+}
+
+func main() {
+	running.SetVersion(version)
+
+	flag.Parse()
+
+	// Show help early if requested (skip config load)
+	if *helpShort || *helpLong {
+		flag.Usage()
+		return
+	}
+
+	if err := conf.Load(); err != nil {
+		log.Fatal("[Main] Error loading config:", err)
+		return
+	}
+
+	lockFile := filepath.Join(conf.Get().Global.DataPath, "server.lock")
+	running.RegisterLockFile(lockFile)
+
+	if running.IsDaemonMode() {
+		fmt.Println("Running in daemon mode")
+		server.Run()
+		if running.IsRestart() {
+			running.StartNewServer()
+		}
+	} else {
+		fmt.Println("Running in non-daemon mode")
+		client.Run()
+	}
+}
